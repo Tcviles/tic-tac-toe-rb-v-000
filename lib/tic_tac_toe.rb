6c3ecf4a2e1853ddafef057788dd0f02544b2dcf
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-04 01:00:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,7 +60,6 @@ def won?(board)
 
     ((position_1 == position_2) && (position_2 == position_3) && (position_taken?(board,win_combination[0]))) ? (return win_combination) : nil
   end
-  false
 end
 
 # def won?(board)
