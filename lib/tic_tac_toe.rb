9bfd7eefa1f08088a94bb3fda875c67ad355c1bb
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-04 00:57:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,7 +58,7 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
-    if ((position_1 == position_2) && (position_2 == position_3) && position_taken?(board,win_combination[0]))
+    ((position_1 == position_2) && (position_2 == position_3) && (position_taken?(board,win_combination[0]))) ? win_combination : nil
       return win_combination
     else
       nil
