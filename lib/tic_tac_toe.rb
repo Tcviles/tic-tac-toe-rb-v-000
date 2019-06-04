809aead5af977ca92855c39ec1b4c662fc335c26
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-04 00:54:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,7 +49,7 @@ def turn(board)
 end
 
 def won?(board)
-  WIN_COMBINATIONS.detect do |win_combination|
+  WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
@@ -58,8 +58,11 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
-    ((position_1 == position_2) && (position_2 == position_3) && (position_taken?(board,win_combination[0]))) ? win_combination : nil
+    ((position_1 == position_2) && (position_2 == position_3) && position_taken?(board,win_combination[0])) ? win_combination : nil
+    end
   end
+
+  false
 end
 
 # def won?(board)
