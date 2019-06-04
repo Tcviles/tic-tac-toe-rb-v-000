b9df5873a966cef745717bd7864e3b75a8b52ae6
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-04 00:40:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,9 +28,7 @@ end
 def turn_count(board)
   turns = 0
   board.each do |position|
-    if position == "X" || position == "O"
-      turns +=1
-    end
+    position == "X" || position == "O" ? turns += 1 : nil
   end
   return turns
 end
